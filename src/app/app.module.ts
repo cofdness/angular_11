6e1720e33ec244fdd9127cb6ac3b55fc076e714e
Vu Tran Kim Phung <vtkp2002dn@gmail.com>
2021-01-01 17:18:35 +0700

    - add some client component - init restful express server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductAlertsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([{
+      path: '', component: ProductListComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
